108ac423385150c215bdaf82421e93e4b56ec7cc
1khvled <1khvled@users.noreply.github.com>
2026-08-03 21:48:32 +0100

    make hosted library work offline

--- website/sw.js
+++ website/sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'downloads-shell-v3';
-const SHELL = ['./', './index.html', './styles.css', './app.js', './config.js', './manifest.webmanifest'];
+const SHELL = ['./', './index.html', './styles.css', './polish.css', './app.js', './config.js', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim().then(() => caches.keys()).then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
