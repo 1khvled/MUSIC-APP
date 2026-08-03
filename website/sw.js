135b7f84a7f96c294be71616a95015039c70ada3
1khvled <1khvled@users.noreply.github.com>
2026-08-03 22:00:03 +0100

    make offline audio cache persistent

--- website/sw.js
+++ website/sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'downloads-shell-v3';
+const CACHE = 'downloads-shell-v4';
 const SHELL = ['./', './index.html', './styles.css', './polish.css', './app.js', './config.js', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim().then(() => caches.keys()).then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim().then(() => caches.keys()).then(keys => Promise.all(keys.filter(key => ![CACHE, 'downloads-media-v1'].includes(key)).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).then(response => {
