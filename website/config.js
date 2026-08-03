c57d0bbe5d502a413810e67193cf2d5cb95110aa
1khvled <1khvled@users.noreply.github.com>
2026-08-03 17:10:12 +0100

    Add mobile website

---
+++ website/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: 'https://yhnekdvxafgdbmbituql.supabase.co',
+  supabasePublishableKey: 'sb_publishable_p8XiGHHNiakXc4IgWY33xw_y1HrUdM2',
+  storageBucket: 'music'
+};
